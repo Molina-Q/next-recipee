64e764ece7099286f37bb4bebf171ae401aa7ac0
Molina-Q <quentinmolina1@gmail.com>
2024-09-23 21:44:27 +0200

    Refactor Home component to fetch and display recipes recipes are being fetched. - Handle error cases and display appropriate error messages.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,65 @@
-import Image from "next/image";
+import { Recipe } from "@prisma/client";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const { recipees, loading, message } = useFetchRecipes();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (message) {
+    return <div>{message}</div>;
+  }
+
   return (
     <div>
-      <h1>Hello</h1>
-      <Link href="/recipees">
-        Go to not home
-      </Link>
+      <h1>Recipes Website !</h1>
+      { message && <p>{message}</p> }
+
+      { 
+        recipees && 
+        <ul>
+          {recipees.map((recipe) => (
+            <li key={recipe.id}>
+              <Link href={`/recipe/${recipe.id}`}>
+                <p>{recipe.title}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      }
+      
     </div>
   );
 }
+
+const useFetchRecipes = () => {
+  const [recipees, setRecipes] = useState<Recipe[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    async function fetchRecipes() {
+      try {
+        const response = await fetch("/api/recipe");
+        const data = await response.json();
+        
+        if (!data.success) {
+          setMessage(data.message);
+        } else {
+          setRecipes(data.data);
+        }
+      } catch (error) {
+        setMessage(String(error));
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchRecipes();
+  }, []);
+
+  return { recipees, loading, message };
+};
