954833535e9f82f175e1d259d02071da9fd5ddcf
Molina-Q <quentinmolina1@gmail.com>
2024-09-27 16:25:52 +0200

    Add ingredient creation form with image upload functionality

---
+++ app/(client)/ingredients/create/page.tsx
@@ -0,0 +1,81 @@
+"use client"
+import { useState } from "react";
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
+import { CldUploadWidget } from 'next-cloudinary';
+import { Label } from "@/components/ui/label";
+
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: "Name is required.",
+    }),
+    image: z.string(),
+});
+
+export default function IngredientForm() {
+    const [ingredient, setIngredient] = useState<z.infer<typeof formSchema>>({
+        name: "",
+        image: '',
+    });
+    console.log(ingredient);
+    
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        try {
+            const response = await fetch("/api/ingredient/create", {
+                method: "POST",
+                body: JSON.stringify(ingredient),
+            });
+
+            const data = await response.json();
+            console.log(data);
+
+        } catch (error) {
+            console.error("Error creating Tool:", error);
+        }
+    }
+
+    async function handleSuccessUpload(result: any) {
+        setIngredient({ 
+            ...ingredient, 
+            image: result.info.secure_url 
+        });
+
+        console.log(result.info.secure_url);
+
+        // form.setValue("image", result.secure_url);
+    }
+
+    async function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const { name, value } = e.target;
+        setIngredient({
+            ...ingredient,
+            [name]: value,
+        });
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="space-y-8 flex flex-col">
+            <div>
+                <Label>Name</Label>
+                <Input type="text" id={"name"} name="name" value={ingredient.name} onChange={handleChange} />
+            </div>
+
+            <CldUploadWidget
+                uploadPreset="next_upload_preset"
+                onSuccess={handleSuccessUpload}
+            >
+                {({ open }) => {
+                    return (
+                        <button type="button" onClick={() => open()}>
+                            Upload an Image
+                        </button>
+                    );
+                }}
+            </CldUploadWidget>
+
+            <Button className="bg-slate-950 text-white" type="submit">Submit</Button>
+        </form>
+    );
+}
