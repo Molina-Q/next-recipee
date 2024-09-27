74f78af10d8e3535dbaa123550fb417274c408c1
Molina-Q <quentinmolina1@gmail.com>
2024-09-27 12:09:08 +0200

    Refactor recipe creation route to include tools and ingredients

--- app/api/recipe/create/route.tsx
+++ app/api/recipe/create/route.tsx
@@ -1,29 +1,59 @@
-// import { db } from '@/lib/db';
-// import { NextRequest, NextResponse } from 'next/server';
+import { db } from '@/lib/db';
+import { NextRequest, NextResponse } from 'next/server';
 
-// export async function POST(
-//     req: NextRequest
-// ) {
-//     try {
-//         const { title, instructions, imageUrl, diff, time, vegan, healthy, steps } = await req.json();
+export async function POST(
+    req: NextRequest
+) {
+    try {
+        const { quantity, unit, tools, ingredients, title, instructions, imageUrl, diff, time, vegan, healthy, steps, categoryId } = await req.json();
 
-//         const recipe = await db.recipe.create({
-//             data: {
-//                 title,
-//                 instructions,
-//                 imageUrl,
-//                 diff,
-//                 time,
-//                 vegan,
-//                 healthy,
-//                 steps
-//             }
-//         });
+        const recipe = await db.recipe.create({
+            data: {
+                title,
+                instructions,
+                imageUrl,
+                diff,
+                time,
+                vegan,
+                healthy,
+                steps,
+                categoryId,
+                userId: "a1",
+            }
+        });
 
-//         return NextResponse.json([recipe]);
+        const recipeTools: unknown[] = [];
+        {
+            tools.map(async (tool: string) => {
+                const recipeTool = await db.recipeTool.create({
+                    data: {
+                        recipeId: recipe.id,
+                        toolId: tool,
+                    }
+                });
+                recipeTools.push(recipeTool);
+            })
+        }
 
-//     } catch (error) {
-//         console.log("error create Movie : ", error);
-//         return new NextResponse("Internal Server Error", { status: 500 });
-//     }
-// }
+        const recipeIngredients: unknown[] = [];
+        {
+            ingredients.map(async (ingredient: string) => {
+                const recipeIngredient = await db.recipeIngredient.create({
+                    data: {
+                        recipeId: recipe.id,
+                        ingredientId: ingredient,
+                        quantity: quantity,
+                        unit: unit,
+                    }
+                });
+                recipeIngredients.push(recipeIngredient);
+            })
+        }
+
+        return NextResponse.json([recipe]);
+
+    } catch (error) {
+        console.log("error create Movie : ", error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
