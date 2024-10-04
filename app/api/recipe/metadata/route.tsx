177cfb0baf4a9460b93e0b4d6f5179433e67d3ec
Molina-Q <quentinmolina1@gmail.com>
2024-10-04 11:36:41 +0200

    Refactor GET route to include recipe metadata in the response

---
+++ app/api/recipe/metadata/route.tsx
@@ -0,0 +1,18 @@
+import { db } from '@/lib/db';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(
+    req: NextRequest
+) {
+    try {
+        const ingredients = await db.ingredient.findMany();
+        const tools = await db.tool.findMany();
+        const categories = await db.category.findMany();
+
+        return NextResponse.json({ingredients, tools, categories});
+
+    } catch (error) {
+        console.log("error getting data : ", error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
