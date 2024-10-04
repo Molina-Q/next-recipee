efad7bfb34fe70d1b85d9dd67cccb60e15ad01b3
Molina-Q <quentinmolina1@gmail.com>
2024-10-04 16:57:11 +0200

    Refactor interface.ts to include RecipeType, RecipeIngredientType, IngredientType, RecipeToolType, and ToolType interfaces

---
+++ app/types/interface.ts
@@ -0,0 +1,30 @@
+import { Recipe } from "@prisma/client";
+
+export interface RecipeType extends Recipe {
+    RecipeIngredients: RecipeIngredientType[];
+    RecipeTools: RecipeToolType[];
+}
+
+export interface RecipeIngredientType {
+    id: number;
+    quantity: number;
+    unit: string;
+    ingredient: IngredientType;
+}
+
+export interface IngredientType {
+    id: number;
+    name: string;
+    imageUrl: string;
+}
+
+export interface RecipeToolType {
+    id: number;
+    tool: ToolType;
+}
+
+export interface ToolType {
+    id: number;
+    name: string;
+    imageUrl: string;
+}
