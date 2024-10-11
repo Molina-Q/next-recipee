4bb4ec9910b7fb677d28d4c2e214ffeed0fb6468
Molina-Q <quentinmolina1@gmail.com>
2024-10-11 15:58:32 +0200

    Refactor recipe page layout and add Tabs component

---
+++ app/(client)/recipes/[recipeId]/_component/Tabs.tsx
@@ -0,0 +1,76 @@
+"use client"
+import React, { useState } from 'react'
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
+import { RecipeType } from '@/app/types/interface'
+import Image from 'next/image'
+type TabsProps = {
+    tabs: string[],
+    panels: RecipeType
+}
+
+const Tabs: React.FC<TabsProps> = ({ tabs, panels }) => {
+    const [selectedIndex, setSelectedIndex] = useState(0)
+    return (
+        <TabGroup selectedIndex={selectedIndex} onChange={setSelectedIndex}>
+            <TabList className="flex gap-4 bg-slate-800 rounded-t-lg">
+                {tabs.map((tab, index) => (
+                    <Tab
+                        key={index}
+                        className={`
+                        text-2xl 
+                        px-2 py-1 
+                        outline-none
+                        data-[selected]:bg-slate-700 
+                        ${index === 0 && "data-[selected]:rounded-tl-lg"} 
+                        data-[selected]:text-white
+                        `}
+                    >
+                        {tab}
+                    </Tab>
+                ))}
+            </TabList>
+
+            <TabPanels className={"border-slate-600 border-2 rounded-b-lg"}>
+                {panels.RecipeIngredients.map((recipeIngredient) => (
+                    <TabPanel key={recipeIngredient.id}>
+                        <div className='my-3 py-3 flex flex-row gap-5 p-3'>
+                            <div key={recipeIngredient.id} className='flex flex-col items-center'>
+                                <Image
+                                    src={recipeIngredient.ingredient.imageUrl}
+                                    height={100}
+                                    width={100}
+                                    alt={recipeIngredient.ingredient.name}
+                                    className='size-[100px] rounded-md object-fill'
+                                />
+                                <p>
+                                    {recipeIngredient.ingredient.name}
+                                </p>
+                                <p>{recipeIngredient.quantity} {recipeIngredient.unit}</p>
+                            </div>
+                        </div>
+                    </TabPanel>
+                ))}
+
+                <TabPanel className="flex flex-row">
+                    {panels.RecipeTools.map((recipeTool) => (
+                        <div className='my-3 py-3 flex flex-row gap-5 p-3' key={recipeTool.id}>
+                            <div key={recipeTool.id} className="flex flex-col items-center">
+                                <Image
+                                    src={recipeTool.tool.imageUrl}
+                                    height={100}
+                                    width={100}
+                                    alt={recipeTool.tool.name}
+                                    className='size-[100px] rounded-md object-fill'
+                                />
+                                <p>{recipeTool.tool.name}</p>
+                            </div>
+                        </div>
+                    ))}
+                </TabPanel>
+
+            </TabPanels>
+        </TabGroup>
+    )
+}
+
+export default Tabs
