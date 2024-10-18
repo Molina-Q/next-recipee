1a780d3c77ea1f581667e89cc04029bad85dcfbb
Molina-Q <quentinmolina1@gmail.com>
2024-10-18 16:55:27 +0200

    Added nextBundle Analyzer and updated config for it

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         remotePatterns: [
@@ -6,7 +11,8 @@ const nextConfig = {
                 hostname: 'res.cloudinary.com',
             },
         ],
-    }
+       
+    },
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
