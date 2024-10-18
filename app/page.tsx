6b62358dbd09dad56d166a42659f146d3d553e24
Molina-Q <quentinmolina1@gmail.com>
2024-10-18 11:59:26 +0200

    Refactor Home component to separate getRecipes function

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,32 @@ import Link from "next/link";
 import SingleRecipeCard from "./_component/SingleRecipeCard";
 import { Recipe } from "@/lib/types";
 
+export default async function Home() {
+  let recipes: Recipe[] | null = null;
+  let error: string | null = null;
+
+  try {
+    recipes = await getRecipes();
+  } catch (e) {
+    error = e instanceof Error ? e.message : String(e);
+  }
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+
+      {recipes && (
+        <div className="flex flex-col justify-center items-center md:flex-row gap-2 ">
+          {recipes.map((recipe) => (
+            <SingleRecipeCard recipe={recipe} key={recipe.id} />
+          ))}
+        </div>
+      )}
+
+    </div>
+  );
+}
+
 async function getRecipes() {
   'use server';
   try {
@@ -18,30 +44,4 @@ async function getRecipes() {
   } catch (error) {
     throw new Error(`Failed to fetch recipes: ${error}`);
   }
-}
-
-export default async function Home() {
-  let recipes: Recipe[] | null = null;
-  let error: string | null = null;
-
-  try {
-    recipes = await getRecipes();
-  } catch (e) {
-    error = e instanceof Error ? e.message : String(e);
-  }
-
-  return (
-    <div>
-      {error && <p>{error}</p>}
-
-      {recipes && (
-        <div className="flex flex-row gap-2">
-          {recipes.map((recipe) => (
-            <SingleRecipeCard recipe={recipe} key={recipe.id} />
-          ))}
-        </div>
-      )}
-
-    </div>
-  );
 }
