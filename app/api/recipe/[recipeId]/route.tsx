c37848dc9a507c751032d6ee0f6c4bf25fdd4d71
Molina-Q <quentinmolina1@gmail.com>
2024-11-08 11:45:05 +0100

    Fetch nutritional data from Edamam API and include it in recipe response

--- app/api/recipe/[recipeId]/route.tsx
+++ app/api/recipe/[recipeId]/route.tsx
@@ -33,7 +33,38 @@ export async function GET(
             return NextResponse.json({ success: false }, { status: 404 });
         }
 
-        return NextResponse.json({ data: recipe, success: true }, { status: 200 });
+        const ingredients = recipe.RecipeIngredients.map(ri => `${ri.quantity} ${ri.unit} ${ri.ingredient.name}`).join('\n');
+        const recipeData = {
+            title: recipe.title,
+            ingr: ingredients.split('\n'),
+        }
+        console.log(JSON.stringify({
+            title: recipe.title,
+            ingr: ingredients.split('\n'),
+        }));
+
+        // Make a POST request to the Edamam API
+        const edamamResponse = await fetch(
+            `https://api.edamam.com/api/nutrition-details?app_id=${process.env.NEXT_EDAMAM_ID}&app_key=${process.env.NEXT_EDAMAM_KEY}`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    title: recipe.title,
+                    ingr: ingredients.split('\n'),
+                }),
+            }
+        );
+
+        if (!edamamResponse.ok) {
+            throw new Error('Failed to fetch nutritional data from Edamam API');
+        }
+
+        const nutritionalData = await edamamResponse.json();
+
+        return NextResponse.json({ recipe: recipe, nutritional: nutritionalData, success: true }, { status: 200 });
 
     } catch (error) {
         console.log("error getting data : ", error);
