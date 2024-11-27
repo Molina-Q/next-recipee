30a7938e57cd4b06315d57d3c672ed8c8eab0676
Molina-Q <quentinmolina1@gmail.com>
2024-11-27 13:31:59 +0100

    Enhance ToolForm with validation, loading state, and toast notifications; improve user feedback on form submission

--- app/(client)/tools/create/page.tsx
+++ app/(client)/tools/create/page.tsx
@@ -5,12 +5,15 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input";
 import { CldUploadWidget } from 'next-cloudinary';
 import { Label } from "@/components/ui/label";
+import { toast } from "react-toastify";
 
 const formSchema = z.object({
     name: z.string().min(1, {
-        message: "Name is required.",
+        message: "Name is required",
     }),
-    image: z.string(),
+    image: z.string().min(1, {
+        message: "Image is required"
+    }),
 });
 
 export default function ToolForm() {
@@ -18,24 +21,53 @@ export default function ToolForm() {
         name: "",
         image: '',
     });
-    
+    const [loading, setLoading] = useState(false);
+
     async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
+        setLoading(true);
         try {
+            const verifyData = formSchema.parse(tool)
+
             const response = await fetch("/api/tool/create", {
                 method: "POST",
-                body: JSON.stringify(tool),
+                body: JSON.stringify(verifyData),
+            });
+
+            toast.success("Created new Tool !", {
+                position: "top-right",
+                autoClose: 4000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
             });
 
         } catch (error) {
+            if (error instanceof z.ZodError) {
+                return toast.error(error.errors.map(err => err.path).join(', ') + " required", {
+                    position: "top-right",
+                    autoClose: 4000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                });
+            }
             console.error("Error creating Tool:", error);
+        } finally {
+            setLoading(false);
         }
     }
 
     async function handleSuccessUpload(result: any) {
-        setTool({ 
-            ...tool, 
-            image: result.info.secure_url 
+        setTool({
+            ...tool,
+            image: result.info.secure_url
         });
     }
 
@@ -49,26 +81,30 @@ export default function ToolForm() {
 
 
     return (
-        <form onSubmit={handleSubmit} className="space-y-8 flex flex-col">
-            <div>
-                <Label>Name</Label>
-                <Input type="text" id={"name"} name="name" value={tool.name} onChange={handleChange} />
-            </div>
+        <>
+            <h1 className="text-center">Create Tools</h1>
 
-            <CldUploadWidget
-                uploadPreset="next_upload_preset"
-                onSuccess={handleSuccessUpload}
-            >
-                {({ open }) => {
-                    return (
-                        <button type="button" onClick={() => open()}>
-                            Upload an Image
-                        </button>
-                    );
-                }}
-            </CldUploadWidget>
+            <form onSubmit={handleSubmit} className="space-y-8 flex flex-col">
+                <div>
+                    <Label>Name</Label>
+                    <Input type="text" id={"name"} name="name" value={tool.name} onChange={handleChange} />
+                </div>
 
-            <Button className="bg-slate-950 text-white" type="submit">Submit</Button>
-        </form>
+                <CldUploadWidget
+                    uploadPreset="next_upload_preset"
+                    onSuccess={handleSuccessUpload}
+                >
+                    {({ open }) => {
+                        return (
+                            <button type="button" onClick={() => open()}>
+                                Upload an Image
+                            </button>
+                        );
+                    }}
+                </CldUploadWidget>
+
+                <Button className="bg-slate-950 text-white" type="submit" disabled={loading}>{ loading ? "Loading..." : "Submit"}</Button>
+            </form>
+        </>
     );
 }
