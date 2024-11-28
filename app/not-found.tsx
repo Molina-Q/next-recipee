cb7ffc4b9f11ef2cab150e859825e70d6f7876ac
Molina-Q <quentinmolina1@gmail.com>
2024-11-28 21:22:09 +0100

    Enhance NotFound component with improved heading styles for better visibility

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,8 @@
-import Link from 'next/link'
- 
 export default function NotFound() {
   return (
     <div className='flex flex-col justify-center items-center'>
-      <h2>Page Not Found</h2>
+      <h1 className="text-7xl">404</h1>
+      <h2 className="text-3xl">Page Not Found</h2>
       <p>Could not the find requested resource</p>
     </div>
   )
